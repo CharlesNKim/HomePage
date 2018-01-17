5f1a73804fc3c0b7ae66ac4df5ebabbd1839dd6f
e <orange7018@naver.com>
2018-01-17 22:03:31 +0900

    add ip tracking

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'CharlesN\'s Page'});
+router.get('/', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log(ip);
+  res.render('index', {
+    title: 'CharlesN\'s Page'
+  });
 });
 
 module.exports = router;
